0f0dc1bd43a5453db0b610f29174f692c00232af
Vladislav Tarasevich <vlad.tarasevich27@gmail.com>
2020-03-23 19:21:55 +0300

    [Chapter 11] Add rows management.

--- 5 - Homepwner/Homepwner/ItemStore.swift
+++ 5 - Homepwner/Homepwner/ItemStore.swift
@@ -14,20 +14,28 @@ final class ItemStore {
 
     var allItems = [Item]()
 
-    // MARK: - Init
-
-    init() {
-        for _ in 0..<5 {
-            createItem()
-        }
-    }
-
     // MARK: - Methods
 
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
         allItems.append(newItem)
         return newItem
+    }
+
+    func removeItem(_ item: Item) {
+        if let index = allItems.firstIndex(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+
+        let movedItem = allItems[fromIndex]
+        allItems.remove(at: fromIndex)
+        allItems.insert(movedItem, at: toIndex)
     }
 
 }
--- 5 - Homepwner/Homepwner/ItemsViewController.swift
+++ 5 - Homepwner/Homepwner/ItemsViewController.swift
@@ -22,6 +22,32 @@ final class ItemsViewController: UITableViewController {
         adjustTableViewContentInset()
     }
 
+    // MARK: - Private methods
+
+    @IBAction private func addNewItem(_ sender: UIButton) {
+        let newItem = itemStore.createItem()
+
+        if let newItemIndex = itemStore.allItems.firstIndex(of: newItem) {
+            let indexPath = IndexPath(row: newItemIndex, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        } else {
+            assertionFailure("Unable to find newItem in itemStore")
+        }
+
+    }
+
+    @IBAction private func toggleEditingMode(_ sender: UIButton) {
+        if isEditing {
+            sender.setTitle("Edit", for: .normal)
+            setEditing(false, animated: true)
+        } else {
+            sender.setTitle("Done", for: .normal)
+            setEditing(true, animated: true)
+        }
+    }
+
+    // MARK: - UITableViewDataSource
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
     }
@@ -36,6 +62,22 @@ final class ItemsViewController: UITableViewController {
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let itemToDelete = itemStore.allItems[indexPath.row]
+
+            let alertController = makeAlertForDeletingItem(withName: itemToDelete.name, deleteHandler: { [weak self] action in
+                self?.itemStore.removeItem(itemToDelete)
+                self?.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+
     // MARK: - Private methods
 
     private func adjustTableViewContentInset() {
@@ -43,6 +85,16 @@ final class ItemsViewController: UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+    }
+
+    private func makeAlertForDeletingItem(withName itemName: String, deleteHandler handler: @escaping ((UIAlertAction) -> Void)) -> UIAlertController {
+        let ac = UIAlertController(title: "Delete \(itemName)?", message: "Are you sure you want to delete this item?", preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        ac.addAction(cancelAction)
+
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: handler)
+        ac.addAction(deleteAction)
+        return ac
     }
 
 }
