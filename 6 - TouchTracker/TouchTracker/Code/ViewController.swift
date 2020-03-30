c8f596bd9d10b8601890e4b1be2c1b2e626341b9
Vladislav Tarasevich <vlad.tarasevich27@gmail.com>
2020-03-31 00:58:22 +0300

    [Chapter 18] Init for TouchTracker app.

---
+++ 6 - TouchTracker/TouchTracker/Code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by Vladislav Tarasevich on 31.03.2020.
+//  Copyright © 2020 Vladislav Tarasevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
